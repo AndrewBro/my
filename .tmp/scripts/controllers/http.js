f1bfb8abc29ad2a4d16fcf8be80a754ad8c37406
AndrewBro <beliverwww@gmail.com>
2017-04-18 11:50:01 +0300

    firstt

---
+++ .tmp/scripts/controllers/http.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/*  @ngdoc function
+  @name myApp.controller:httpCtrl
+  @description
+  # httpCtrl
+  Controller of the myApp
+*/
+
+angular.module('myApp', []).controller('httpCtrl', function ($scope, $http) {
+  $http.get('http://localhost:9000/data/data.json').success(function (response) {
+    $scope.persons = response.people;
+  });
+  this.awesomeThings = ['HTML5 Boilerplate', 'AngularJS', 'Karma'];
+});
+//# sourceMappingURL=http.js.map
