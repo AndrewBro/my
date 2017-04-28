debd36617e730e81c480ac2fe9a6d9fe440ebd5d
AndrewBro <beliverwww@gmail.com>
2017-04-28 17:16:24 +0300

    ignore without .tmp

---
+++ .tmp/scripts/components/movie-block/movie-block.js
@@ -0,0 +1,30 @@
+"use strict";
+
+(function e(t, n, r) {
+  function s(o, u) {
+    if (!n[o]) {
+      if (!t[o]) {
+        var a = typeof require == "function" && require;if (!u && a) return a(o, !0);if (i) return i(o, !0);throw new Error("Cannot find module '" + o + "'");
+      }var f = n[o] = { exports: {} };t[o][0].call(f.exports, function (e) {
+        var n = t[o][1][e];return s(n ? n : e);
+      }, f, f.exports, e, t, n, r);
+    }return n[o].exports;
+  }var i = typeof require == "function" && require;for (var o = 0; o < r.length; o++) {
+    s(r[o]);
+  }return s;
+})({ 1: [function (require, module, exports) {
+    angular.module('myApp').component('movieBlock', {
+      bindings: {
+        movie: '<'
+        // movieId
+      },
+      templateUrl: '/scripts/components/movie-block/movie-block.html'
+    });
+
+    // postsService.getFullMovie(movie.imdbID)
+    //   .then(function (resp) {
+    //     vm.movies[index] = resp.data;
+    //     // vm.movies[index].isDataLoaded = true;
+    //   })
+  }, {}] }, {}, [1]);
+//# sourceMappingURL=movie-block.js.map
